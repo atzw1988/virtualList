26c2572daa58f2d15156217d20f35baae0a1b1aa
zhangwei <386553646@qq.com>
2020-07-16 11:41:02 +0800

    虚拟滚动

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <VirtualList :fullyData="fullyData" />
+  </div>
+</template>
+
+<script>
+import VirtualList from './components/VirtualList.vue'
+export default {
+  name: 'app',
+  components: {
+    VirtualList
+  },
+  data() {
+    return {
+      fullyData: []
+    }
+  },
+  created() {
+    let arr = []
+    const randomColor = () => {
+      return Math.floor(Math.random() * 255)
+    }
+    for (let i = 0; i < 10000; i ++) {
+      arr.push({
+        value: i,
+        backColor: `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`
+      })
+    }
+    this.fullyData = arr
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="list" @scroll="handleScroll">
+    <div class="item" v-for="(item, index) in list" :key="index">
+      <div class="content">第{{index + 1}}条{{item}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  methods: {
+    click (e) {
+      console.log(e)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
